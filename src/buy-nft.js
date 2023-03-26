ef514b4c6aa713a886f26ac45aa163eb26be9647
Talgat Shakidaev <talgat.shakidaev0312@gmail.com>
2023-03-26 23:33:14 +0900

    Adding the option

--- src/buy-nft.js
+++ src/buy-nft.js
@@ -9,7 +9,27 @@ window.Buffer = Buffer; // needed to use `signSmartContractData` in browser
 
 /* We advise you not to use the private key on the frontend
     It is used only as an example
+
+
 */
+
+const options = {
+  partner_id: 'default',
+  container_id: 'widget',
+  click_id: uuidv4(), // unique id of purhase in your system
+  width: 400,
+  height: 600,
+  currency: 'USD',
+  commodity: 'ETH',
+  commodities: 'BTC,ETH',
+  // currency_amount: 100,
+  listeners: {
+    loaded: () => console.log('loaded'),
+  },
+};
+
+console.log(options);
+
 if (window.ethereum) {
   (async () => {
     // Get user address
